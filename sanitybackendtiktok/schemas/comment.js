be714e7851766cad54fea3a18fb93cce001e35a9
Ivan Gasques <100533246+IvanGasques@users.noreply.github.com>
2023-02-28 09:47:22 +0000

    Start My TikTok
    my backend witch Sanity

---
+++ sanitybackendtiktok/schemas/comment.js
@@ -0,0 +1,18 @@
+export default {
+    name:"comment",
+    title:"Comment",
+    type:"document",
+    fields: [
+        {
+            name:"posteBy",
+            title:"Posted By",
+            type:"postedBy",
+        },
+        {
+            name:"comment",
+            title:"Comment",
+            type:"string",
+        }
+    ]
+
+}
